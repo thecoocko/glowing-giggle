e34aca125af57d3ebefb72934d221fa33b06524f
thecoocko <butenkonaddezhda@gmail.com>
2022-10-21 16:41:12 +0200

    done

--- Modules/madlibsgenerator.py
+++ Modules/madlibsgenerator.py
@@ -1,8 +1,15 @@
+import re
+
+from psutil import users
+
 
 class Reader:
     def __init__(self,file):
         self.file = file
     
+    def __str__(self):
+        return self.file_reader_txt() 
+
     def file_reader_txt(self):
         text = ''
         with open(self.file, 'r',encoding='utf-8') as buffer:
@@ -13,5 +20,23 @@ class Reader:
 class MadLibsGenerator(Reader):
     def __init__(self, file,user_text):
         super().__init__(file)
+        self.user_text = user_text
+
+    def func(self):
+        file_text = str(Reader('text.txt'))
+        underline = [_.start() for _ in re.finditer("_", file_text)] 
+        file_text = list(file_text)
+
+        for i in range(len(underline)):
+            file_text[underline[i]] = self.user_text[i]
+            
+        return "".join(file_text)
+
+            
+        
+        
+
+        
+
 
 
--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-from Modules.guessthenumber import Guesser, gameInterface
-from Modules.madlibsgenerator import Reader
+from Modules.guessthenumber import  gameInterface
+from Modules.madlibsgenerator import MadLibsGenerator
 
 
 def main():
-   #gameInterface()
-    text = Reader('text.txt')
-    print(text.file_reader_txt())
+   gameInterface()
+   def userInput():
+    inp = input("Please enter only 6 words below! (in one sentence) \n")
+    return inp.split()
+   
+   text = MadLibsGenerator('text.txt',user_text=userInput())
+   print(text.func())
     
 
 if __name__ == '__main__':
